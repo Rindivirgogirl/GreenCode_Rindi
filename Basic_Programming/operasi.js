186731adb2445cd0a6c283fe1066a1487f77d47c
Rindivirgogirl <rindicutee325@gmail.com>
2023-10-02 17:48:36 +0700

    add .

---
+++ Basic_Programming/operasi.js
@@ -0,0 +1,22 @@
+let firstName = "Rindi"
+let lastName = "Ani"
+let fullName = firstName + " " +lastName
+
+let angka1 = 12
+let angka2 = 7
+let penjumlahan = angka1 + angka2
+let angka4 = 7
+let angka5 = 4
+let pengurangan = angka4 - angka5
+let perkalian = angka1 * angka2
+let pembagian = angka4 / angka5
+let sisaBagi = angka1 % angka2
+
+console.log(penjumlahan)
+console.log(pengurangan)
+console.log(perkalian)
+console.log(pembagian)
+console.log(sisaBagi)
+
+
+console.log(fullName)
---
+++ Basic_Programming/variabel.js
@@ -0,0 +1,10 @@
+var myname ="rindi"
+let myAge ="20"
+const myAdress ="perdagangan"
+
+let isMahasiswa = true
+let myPrince
+let myPrincess = null
+console.log(isMahasiswa)
+console.log(myPrince)
+console.log(myPrincess)
