1f03d1edb80a5c631d8bfa88f6bf632f086c3a02
Rindivirgogirl <rindicutee325@gmail.com>
2023-10-04 14:32:51 +0700

    add operasi aritmatika

---
+++ Basic_Programming/operasi/aritmatika.js
@@ -0,0 +1,13 @@
+var x=10
+var y=5
+let pertambahan= x + y
+let pengurangan= x - y
+let perkalian= x * y
+let pembagian= x / y
+let sisabagi= x % y
+
+console.log("x + y =", pertambahan)
+console.log("x - y =", pengurangan)
+console.log("x * y =", perkalian)
+console.log("x / y =", pembagian)
+console.log("x % y =", sisabagi)
