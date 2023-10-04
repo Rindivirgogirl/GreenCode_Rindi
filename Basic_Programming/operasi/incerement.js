68683c67d760e90344bac663a90ad5b05d21dde2
Rindivirgogirl <rindicutee325@gmail.com>
2023-10-04 15:09:30 +0700

    add perbandingan

---
+++ Basic_Programming/operasi/incerement.js
@@ -0,0 +1,16 @@
+var z= 20
+ //init perkalian 3//
+z *= 3
+//init pertambahan 3//
+z += 3
+//init pengurangan 3//
+z -= 3
+//init pembagian 3//
+z /= 3
+//init sisabagi 3//
+z %= 3
+
+console.log(z)
+
+Boolean = false + false
+console.log(Boolean)
---
+++ Basic_Programming/operasi/perbandingan.js
@@ -0,0 +1,25 @@
+//init perbandingan lebih besar//
+let perbandingan= 5 > 3
+//init perbandingan lebih kecil//
+let lebihkecil = 2 < 4
+//init perbandingan lebih besar sama dengan//
+let lebihbesarsamadengan= 8 >= 4
+//init perbandingan lebih kecil sama dengan//
+let lebihkecilsamadengan= 4 <= 8
+//init perbandingan sama dengan sama dengan//
+let samadengansamadengan= 6 == 4
+//init perbandingan tanpa tipe data//
+let tanpatipedata= 8===2
+//init perbandingan tidak sama dengan//
+let tidaksamadengan= 4 != 2
+//init perbandingan tidak sama dengan sama dengan//
+let tidaksamadengansamadengan= 3 !== 1
+
+console.log(perbandingan)
+console.log(lebihkecil)
+console.log(lebihbesarsamadengan)
+console.log(lebihkecilsamadengan)
+console.log(samadengansamadengan)
+console.log(tanpatipedata)
+console.log(tidaksamadengan)
+console.log(tidaksamadengansamadengan)
