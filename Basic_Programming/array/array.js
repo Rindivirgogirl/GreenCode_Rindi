687cd5aaef8c440009ff4db1434a4cd17d683566
Rindivirgogirl <rindicutee325@gmail.com>
2023-10-04 17:09:04 +0700

    add array

---
+++ Basic_Programming/array/array.js
@@ -0,0 +1,7 @@
+let Wacana= ["rindi", "novi", "maylani", "inne" ]
+let Angka= [1, 2, 3, 4, 5]
+let penjumlahan= Angka[1] + Angka[4]
+console.log(Wacana[0])
+console.log(Angka[1])
+console.log(Angka[4])
+console.log(penjumlahan)
