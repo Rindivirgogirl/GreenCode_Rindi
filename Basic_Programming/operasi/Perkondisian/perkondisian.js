fce2b3d36a12c2fc1034cadccff78ac9b578fd5d
Rindivirgogirl <rindicutee325@gmail.com>
2023-10-09 16:42:58 +0700

    add perkondisian

---
+++ Basic_Programming/operasi/Perkondisian/perkondisian.js
@@ -0,0 +1,30 @@
+let mahasiswa = true
+if ("mahasiswa == true") {
+    console.log("aku mahasiswa")
+}else{
+    console.log("aku bukan mahasiswa")
+}
+    
+let nilai = 1
+let inMahasiswa = false
+
+if (inMahasiswa == true) {
+    nilai++
+}else{
+    nilai--
+}
+console.log (nilai)
+    
+let angka = 75
+let hasil = "nilaiku"
+
+if (angka >90) {
+    hasil = "nilai sangat bagus"
+
+}else if (angka <80 && angka >70){
+    hasil = "nilai cukup"
+
+}else if (angka <70){
+    hasil = "nilai jelek"
+}
+    console.log(hasil)
