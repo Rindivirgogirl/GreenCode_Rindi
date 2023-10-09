9f0f91951fd350fd78e53628f16260b4c182b265
Rindivirgogirl <rindicutee325@gmail.com>
2023-10-09 17:36:56 +0700

    add perulangan

---
+++ Basic_Programming/Perulangan/Perulangan.js
@@ -0,0 +1,12 @@
+for (let index = 0; index <100; index++) {
+    console.log(index+1)
+}
+
+for (let index = 1; index <=100 ; index += 2) {
+    console.log(index)
+}
+
+let fruits =["mangga", "rambutan", "jambu"]
+for (let index = 0; index < 3; index++) {
+    console.log(fruits[index])  
+}
